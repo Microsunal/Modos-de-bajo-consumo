26b3ebf9111079da612b26753b2c155a094a638d
Micros Unal <50812478+Microsunal@users.noreply.github.com>
2019-06-03 21:43:34 -0500

    Add files via upload

---
+++ 4-bajo_consumo-Stop/FLASH/Sources/main_asm.d
@@ -0,0 +1,5 @@
+
+../Sources/main.o: \
+	../Sources/main.asm \
+	C:/Users/Ivan/MyNewWorkspace/4_1-bajo_consumo_2/Project_Headers/derivative.inc \
+	C:/Users/Ivan/MyNewWorkspace/4_1-bajo_consumo_2/Project_Headers/MC9S08QG8.inc 
---
+++ 4-bajo_consumo-Wait/FLASH/Sources/main_asm.d
@@ -0,0 +1,5 @@
+
+../Sources/main.o: \
+	../Sources/main.asm \
+	C:/Users/Ivan/MyNewWorkspace/4-bajo_consumo/Project_Headers/derivative.inc \
+	C:/Users/Ivan/MyNewWorkspace/4-bajo_consumo/Project_Headers/MC9S08QG8.inc 
